784d89e17c1a6d73f29fe427ade49b51dc2161f1
YeHt3t <ye392008@gmai.com>
2024-04-29 11:34:45 +0700

    29,4,24

--- test15.py
+++ test15.py
@@ -1,3 +1,13 @@
 Buy = ['marshel', 'vandel', 'ghost', 'guardian', ' shorty', 'odin', 'stinger']
 print(Buy)
 print(type(Buy))
+
+print(Buy[0])
+
+print(Buy[3])
+
+print(Buy[:4])
+
+print(Buy[2:])
+
+print(Buy[3:6])
