af10b97bba633b267722a101131bfb7cd13abaa8
YeHt3t <ye392008@gmai.com>
2024-04-29 11:42:32 +0700

    29,4,24

--- test15.py
+++ test15.py
@@ -2,15 +2,15 @@ Buy = ['marshel', 'vandel', 'ghost', 'guardian', ' shorty', 'odin', 'stinger']
 print(Buy)
 print(type(Buy))
 
-print(Buy[0])
+# print(Buy[0])
 
-print(Buy[3])
+# print(Buy[3])
 
-print(Buy[:4])
+# print(Buy[:4])
 
-print(Buy[2:])
+# print(Buy[2:])
 
-print(Buy[3:6])
+# print(Buy[3:6])
 
 if 'ghost' in Buy:
     print("Bought")
