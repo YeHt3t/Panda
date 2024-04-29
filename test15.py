dad121cfb18f597451fe0f1fe31fd5637dd7cfd0
YeHt3t <ye392008@gmai.com>
2024-04-29 11:53:24 +0700

    29,4,24

--- test15.py
+++ test15.py
@@ -1,7 +1,10 @@
 Buy = ['marshel', 'vandel', 'ghost', 'guardian', ' shorty', 'odin', 'stinger']
 
-Buy.insert(0, "classic")
+Buy[2] = 'sheriff'
 print(Buy)
+
+# Buy.insert(0, "classic")
+# print(Buy)
 
 
 
@@ -9,15 +12,15 @@ print(Buy)
 # print(Buy)
 # print(type(Buy))
 
-# # print(Buy[0])
+# print(Buy[0])
 
-# # print(Buy[3])
+# print(Buy[3])
 
-# # print(Buy[:4])
+# print(Buy[:4])
 
-# # print(Buy[2:])
+# print(Buy[2:])
 
-# # print(Buy[3:6])
+# print(Buy[3:6])
 
 
 
