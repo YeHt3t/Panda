2d2968ec7676af7f998ac11651fafe6223741d52
YeHt3t <ye392008@gmai.com>
2024-04-30 12:24:51 +0700

    30, 4, 24

---
+++ test20.py
@@ -0,0 +1,4 @@
+def info(name, mail, addr="Inazuma"):
+    print("Your name is ", name)
+    print("Your mail is ", mail)
+    print("Your addr is ", addr)
---
+++ test21.py
@@ -0,0 +1,10 @@
+from test20 import info
+
+#Postional
+info("Ye Htet", "ye1234@gmail.com", "Berlin")
+
+#keyword
+info(name="Auzar", addr="Sydney", mail="auzar.2@gmail.com")
+
+#default
+info("peanut", "raiden@gmail.com")
