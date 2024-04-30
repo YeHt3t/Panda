9da84b4a95afed2a07779eb3766814801f7206e7
YeHt3t <ye392008@gmai.com>
2024-04-30 11:42:23 +0700

    30, 4, 24

---
+++ test17.py
@@ -0,0 +1,19 @@
+info = {
+    'name' : 'Japan',
+    'type' : 'Island',
+    'Likeable Parts' : {
+        'one' : 'Culture',
+        'two' : 'Food',
+        'three' : 'Anime',
+        'four' : 'respect'
+
+    },
+    'cons' : {
+        'one' : 'population',
+        'two' : 'International isolation'
+    }
+
+}
+print(info['name'])
+print(info['Likeable Parts']['three'])
+print(info['cons']['two'])
