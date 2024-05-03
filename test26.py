3ceb1fbd0a7ac224326403d712162cffeea7c73d
YeHt3t <ye392008@gmai.com>
2024-05-03 23:15:33 +0700

    shop

--- test26.py
+++ test26.py
@@ -1,25 +1,27 @@
 class Shop():
     
-    def __init__(link, name, weapon, creds=800):
-        link.name = name
-        link.weapon = weapon
-        link.weapon = creds
+    def __init__(self, name, weapon, creds=800):
+        self.name = name
+        self.weapon = weapon
+        self.creds = creds
 
-    def buy_sherif(link):
-        print(link.name + " bought a sherif")
-        print(link.name + " has 0 creds left")
+    def buy_sherif(self):
+            print(self.name + " bought a sheriff")
+            self.creds -= 800
+            print(self.name + " has", self.creds, "creds left")
 
-    def buy_spell(link):
-        print(link.name + " bought a spell")
-        print(link.name + " has 300 creds left")
+    def buy_spell(self):
+            print(self.name + " bought a spell")
+            self.creds -= 200
+            print(self.name + " has ", self.creds, " creds left")
+    def buy_ghost(self):
+            print(self.name + " bought a ghost")
+            self.creds -= 500
+            print(self.name + " has", self.creds, "creds left")
 
-    def buy_ghost(link):
-        print(link.name + " bought a ghost")
-        print(link.name + " has 200 creds left")
-
-    def buy_shorty(link):
-        print(link.name + " bought a shorty")
-        print(link.name + " has 500 creds left")
-
+    def buy_shorty(self):
+            print(self.name + " bought a shorty")
+            self.creds -= 300
+            print(self.name + " has", self.creds, "creds left")
 
 
--- test27.py
+++ test27.py
@@ -1,13 +1,41 @@
 from test26 import Shop 
 
-c1 = Shop("Panda", "sherif")
-c1.buy_sherif()
+# c1 = Shop("Panda", "sherif")
+# c1.buy_sherif()
 
-c2  = Shop("AuzB0t", "ghost")
-c2.buy_ghost()
+# c2  = Shop("AuzB0t", "ghost")
+# c2.buy_ghost()
 
-c3 = Shop("CLive", "spell")
-c3.buy_spell()
+# c3 = Shop("CLive", "spell")
+# c3.buy_spell()
 
-c4 = Shop("Eli", "shorty")
-c4.buy_shorty()
+# c4 = Shop("Eli", "shorty")
+# c4.buy_shorty()
+
+name = input("""
+             
+                         Enter Username 
+                           
+             """)
+
+choice = input("""
+               
+                     What would you like to buy
+               spell     shorty    sheriff   ghost
+                200       300        800      500
+                       You have 800 creds
+               
+               """)
+if choice == "spell":
+    c1 = Shop(name, "spell")
+    c1.buy_spell()
+if choice == "shorty":
+    c2 = Shop(name, "shorty")
+    c2.buy_shorty()
+if choice == "sheriff":
+    c3 = Shop(name, "sheriff")
+    c3.buy_sherif()
+if choice == "ghost":
+    c3 = Shop(name, "ghost")
+    c3.buy_ghost()
+
